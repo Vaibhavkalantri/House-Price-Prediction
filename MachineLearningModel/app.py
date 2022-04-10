375c13580890a3a7b04e6c88aa949152d9309879
hrishikesh2708 <hrishith27@gmail.com>
2022-04-10 18:51:41 +0530

    commit

--- MachineLearningModel/app.py
+++ MachineLearningModel/app.py
@@ -36,27 +36,13 @@ def predict():
     Sqft_Living15 = request.form["Sqft_Living15"]   
     Sqft_Lot15 = request.form["Sqft_Lot15"]
 
-    
-    
-
-#     arr = np.array([Bathrooms,Bedrooms,Condition,Floors,Lat,Long,Sqft_Above,Sqft_Basement,Sqft_Living,Sqft_Living15,Sqft_Lot,Sqft_Lot15,View,
-# Yr_Built,Yr_Renovated])
-    # df = pd.DataFrame({"Bathrooms" : Bathrooms,"Bedrooms" : Bedrooms ,"Sqft_Living": Sqft_Living,"Sqft_Lot" :Sqft_Lot,"Floors" : Floors,"Waterfront":Waterfront,"View" :View,"Condition" : Condition, "Sqft_Above" :Sqft_Above,"Sqft_Basement" :Sqft_Basement,"Yr_Built" : Yr_Built,"Yr_Renovated" : Yr_Renovated, "Lat" : Lat,"Long" :Long,"Sqft_Living15" :Sqft_Living15, "Sqft_Lot15" :Sqft_Lot15})
-
-    # df = np.array([Bathrooms,Bedrooms,Sqft_Living,Sqft_Lot,Floors,Waterfront,View,Condition,Sqft_Above,Sqft_Basement,Yr_Built,Yr_Renovated,Lat,Long,Sqft_Living15,Sqft_Lot15])
-    # df = df.astype(np.float64)
+    df = np.array([Bathrooms,Bedrooms,Sqft_Living,Sqft_Lot,Floors,Waterfront,View,Condition,Sqft_Above,Sqft_Basement,Yr_Built,Yr_Renovated,Lat,Long,Sqft_Living15,Sqft_Lot15])
+    df = df.astype(np.float64)
     s_scaler = StandardScaler()
-    # print("data idher hai ",df)
-    # single_house = s_scaler.transform(df.reshape(-1,16))
-    # print("data mein loacha hai ", single_house)
-
-    arr = np.array([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]) 
-    single_house = s_scaler.transform(arr.reshape(-1,16))
-    y_pred = model.predict(single_house)
-    print(y_pred)
-    # prediction = model.predict(single_house)
+    single_house = s_scaler.fit_transform(df.reshape(-1,16))
+    prediction = model.predict(single_house)
   
-    # return render_template('index.html', Predict_score ='Your house estimate price is  ₹ {} lakhs'.format(prediction))
+    return render_template('predict.html', prediction = prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
